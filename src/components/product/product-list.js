14f3c716759ab5e936b265b0c234e7edbd4b2359
elvinlab <60045534+elvinlab@users.noreply.github.com>
2020-10-15 00:03:04 -0600

    avanzando en guardar productos

--- src/components/product/product-list.js
+++ src/components/product/product-list.js
@@ -7,15 +7,18 @@ export default class productList extends Component {
     
     constructor(props) {
         super(props);
-        this.onChangeCategory = this.onChangeCategory.bind(this);
+        this.onChangeCategoryName = this.onChangeCategoryName.bind(this);
+        this.getCategoryName = this.getCategoryName.bind(this);
 
         this.state = {
         products: null,
         isLoading: null,
 
-        currentCategory: {
-            id: null,
+        currentProduct: {
+            categoryId: null,
+            categoryName: "",
             name: "",
+            price: "",
             description: "",
           },
           message: ""
@@ -26,17 +29,40 @@ export default class productList extends Component {
         this.getProducts();
     }
 
+     onChangeCategoryName(e) {
 
-    onChangeCategory(e) {
-        const name = e.target.value;
-        
-        this.setState(prevState => ({
-            currentCategory: {
-            ...prevState.currentCategory,
-            name: name
-          }
-        }));
-      }
+        const categoryName = e.target.value;
+
+            this.setState(prevState => ({
+                currentProduct: {
+                ...prevState.currentProduct,
+                categoryName: categoryName
+              }
+            }));
+
+    }
+
+    async getCategoryName(e) {
+       
+        const categoryName = e.target.value;
+
+        e.preventDefault();
+
+        const resp = await getCategory("category", categoryName);
+
+        if (resp.status === "success") {
+            console.log(resp.data[0].id);
+            this.setState(prevState => ({
+                currentProduct: {
+                ...prevState.currentProduct,
+                categoryId : resp.data[0].id
+              }
+            }));
+        } else {
+            Swal.fire("Oops", 'No se encontro dicha categoria', "error");
+        }
+     
+    }
 
   async getProducts() {
 
@@ -55,17 +81,18 @@ export default class productList extends Component {
   }
 
   render() {
+    const { currentProduct } = this.state;
     return (
       <div>
         {this.state.isLoading && <span>Cargando productos</span>}
 
         {this.state.products && (
           <div>
-            <h3 class="text-center">Productos</h3>
+            <h3 className="text-center">Productos</h3>
 
             <button
               type="button"
-              class="btn btn-dark text-white"
+              className="btn btn-dark text-white"
               data-toggle="modal"
               data-target="#exampleModalScrollable"
             >
@@ -74,160 +101,83 @@ export default class productList extends Component {
             <br></br>
             <br></br>
             <div
-              class="modal fade"
+              className="modal fade"
               id="exampleModalScrollable"
-              tabindex="-1"
               role="dialog"
               aria-labelledby="exampleModalScrollableTitle"
               aria-hidden="true"
             >
-              <div class="modal-dialog modal-dialog-scrollable" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h6 class="modal-title" id="exampleModalScrollableTitle">
+              <div className="modal-dialog modal-dialog-scrollable" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h6 className="modal-title" id="exampleModalScrollableTitle">
                       Agregar Producto
                     </h6>
                     <button
                       type="button"
-                      class="close"
+                      className="close"
                       data-dismiss="modal"
                       aria-label="Close"
                     >
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
+                  <div className="modal-body">
                     <form>
-                      <div class="form-group">
-                        <label for="nombre">Nombre</label>
+                      <div className="form-group">
+                        <label htmlFor="nombre">Nombre</label>
                         <input
                           name="nombre"
                           type="text"
-                          class="form-control"
+                          className="form-control"
                           id="nombre"
                         ></input>
                       </div>
 
-                      <div class="form-group">
-                        <label for="precio">Precio</label>
+                      <div className="form-group">
+                        <label htmlFor="precio">Precio</label>
                         <input
                           name="precio"
                           type="number"
-                          class="form-control"
+                          className="form-control"
                           id="precio"
                         ></input>
                       </div>
 
-                      <div class="form-group">
-                        <label for="cantidad">Cantidad</label>
+                      <div className="form-group">
+                        <label htmlFor="cantidad">Cantidad</label>
                         <input
                           name="cantidad"
                           type="number"
-                          class="form-control"
+                          className="form-control"
                           id="cantidad"
                         ></input>
                       </div>
 
-                      <div class="form-group">
-                        <label for="categoria">Categoria</label>
+                      <div className="form-group">
+                        <label htmlFor="categoria">Categoria</label>
                         <input
                           name="categoria"
                           type="categoria"
-                          class="form-control"
-                          id="categoria"
+                          className="form-control"
+                          value= {currentProduct.categoryName}
+                          onChange={this.onChangeCategoryName}
                         ></input>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div
-              class="modal fade"
-              id="ModalEditar"
-              tabindex="-1"
-              role="dialog"
-              aria-labelledby="exampleModalScrollableTitle"
-              aria-hidden="true"
-            >
-              <div class="modal-dialog modal-dialog-scrollable" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h6 class="modal-title" id="exampleModalScrollableTitle">
-                      Editar Producto
-                    </h6>
-                    <button
-                      type="button"
-                      class="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <form>
-                      <div class="form-group">
-                        <input
-                          name="codigoProductou"
-                          hidden=""
-                          type="text"
-                          class="form-control"
-                          id="codigoProductou"
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="nombre">Nombre</label>
-                        <input
-                          name="nombre"
-                          type="text"
-                          class="form-control"
-                          id="nombreu"
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="precio">Precio</label>
-                        <input
-                          name="precio"
-                          type="number"
-                          class="form-control"
-                          id="preciou"
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="cantidad">Cantidad</label>
-                        <input
-                          name="cantidad"
-                          type="number"
-                          class="form-control"
-                          id="cantidadu"
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="categorias">Tipo de Producto</label>
-                        <input
-                          disabled
-                          name="categoria"
-                          type="text"
-                          class="form-control"
-                          id="categoriasu"
-                        ></input>
-                      </div>
-                      <div class="form-group">
-                        <label for="file">Fotografia</label>
-                        <input
-                          type="file"
-                          class="form-control-file"
-                          id="photou"
-                        ></input>
+                        <button
+                        className="badge badge-success mr-2"
+                        value= {currentProduct.categoryName}
+                        onClick={this.getCategoryName}
+                        >
+                        Buscar
+                        </button>
                       </div>
                     </form>
                   </div>
-                  <div class="modal-footer">
+                  <div className="modal-footer">
                     <button
                       type="button"
-                      class="btn btn-secondary"
+                      className="btn btn-secondary"
                       data-dismiss="modal"
                     >
                       Cerrar
@@ -236,7 +186,109 @@ export default class productList extends Component {
                       name="agregar"
                       id="realizado2"
                       type="submit"
-                      class="btn btn-light"
+                      className="btn btn-primary"
+                      value="Agregar"
+                      data-dismiss="modal"
+                    ></input>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="modal fade"
+              id="ModalEditar"
+ 
+              role="dialog"
+              aria-labelledby="exampleModalScrollableTitle"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-dialog-scrollable" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h6 className="modal-title" id="exampleModalScrollableTitle">
+                      Editar Producto
+                    </h6>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <div className="form-group">
+                        <input
+                          name="codigoProductou"
+                          hidden=""
+                          type="text"
+                          className="form-control"
+                          id="codigoProductou"
+                        ></input>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                          name="nombre"
+                          type="text"
+                          className="form-control"
+                          id="nombreu"
+                        ></input>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="precio">Precio</label>
+                        <input
+                          name="precio"
+                          type="number"
+                          className="form-control"
+                          id="preciou"
+                        ></input>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="cantidad">Cantidad</label>
+                        <input
+                          name="cantidad"
+                          type="number"
+                          className="form-control"
+                          id="cantidadu"
+                        ></input>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="categorias">Tipo de Producto</label>
+                        <input
+                          disabled
+                          name="categoria"
+                          type="text"
+                          className="form-control"
+                          id="categoriasu"
+                        ></input>
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="file">Fotografia</label>
+                        <input
+                          type="file"
+                          className="form-control-file"
+                          id="photou"
+                        ></input>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Cerrar
+                    </button>
+                    <input
+                      name="agregar"
+                      id="realizado2"
+                      type="submit"
+                      className="btn btn-light"
                       value="Editar"
                       data-dismiss="modal"
                     ></input>
@@ -245,8 +297,8 @@ export default class productList extends Component {
               </div>
             </div>
 
-            <div id="divTabla" class="table-responsive">
-              <table id="tabla" class="table table-hover">
+            <div id="divTabla" className="table-responsive">
+              <table id="tabla" className="table table-hover">
                 <thead>
                   <tr>
                     <th scope="col">ID</th>
@@ -269,14 +321,14 @@ export default class productList extends Component {
                       <td>{product.description}</td>
                       <td>
                         <button
-                          class="btn btn-outline-warning"
+                          className="btn btn-outline-warning"
                           data-toggle="modal"
                           data-target="#ModalEditar"
                         >
-                          <i class="fas fa-edit">Editar</i>
+                          <i className="fas fa-edit">Editar</i>
                         </button>
-                        <button class="btn btn-outline-danger">
-                          <i class="fas fa-trash-alt">Eliminar</i>
+                        <button className="btn btn-outline-danger">
+                          <i className="fas fa-trash-alt">Eliminar</i>
                         </button>
                       </td>
                     </tr>
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -6,11 +6,11 @@ const baseUrl = process.env.REACT_APP_API_URL;
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAxQ0VDMTkwLTBCMzgtMTFFQi1CRDg4LTIxQTJBOTE2RjJDNSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYwMjM2MjI1NCwiZXhwIjoxNjA0OTU0MjU0fQ.qe_inikq-F-2wNVbIAdmbTvNDZnhpA5c9RT31ALEBhU';
 
 
-export const getCategory= ( endpoint ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
-    return axios.get( `${ baseUrl }/${ endpoint }`, { headers: {'Content-Type': 'application/json', 'Authorization': token}} )
+export const getCategory= ( endpoint, categoryName ) => { // por el momento solo un parametro luego tendremos que mandar el token
+
+    return axios.get( `${ baseUrl }/${ endpoint }/${ categoryName }`, { headers: {'Content-Type': 'application/json', 'Authorization': token}} )
     .then(response => {
-        
+ 
     return response.data;
 
     })
