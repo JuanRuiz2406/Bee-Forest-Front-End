760a21249a1edf97f4acbb4781c0d1e1dba4e732
dvperga <44186637+dvperga@users.noreply.github.com>
2020-11-10 22:30:15 -0600

    token

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,107 +3,107 @@ import axios from 'axios';
 const baseUrl = process.env.REACT_APP_API_URL;
 
 //saliendo del apuro
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAxQ0VDMTkwLTBCMzgtMTFFQi1CRDg4LTIxQTJBOTE2RjJDNSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYwMjM2MjI1NCwiZXhwIjoxNjA0OTU0MjU0fQ.qe_inikq-F-2wNVbIAdmbTvNDZnhpA5c9RT31ALEBhU';
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjBCMzlBQ0MwLTIzRDYtMTFFQi05MzlELUUzNjc1MEQxRDlBMiIsInVzZXJuYW1lIjoiZHZwZXJnYSIsImVtYWlsIjoiZGllZ29AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjA1MDY4Nzk3LCJleHAiOjE2MDc2NjA3OTd9.ud-hY6kHE1O4a-dPyorw4PvHgzvF2B130y2xe1y6ozQ';
 
 
 
 
-export const storeProducts = ( endpoint, product ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
+export const storeProducts = (endpoint, product) => { // por el momento solo un parametro luego tendremos que mandar el token
+
     let json = JSON.stringify(product);
     let params = "json=" + json;
 
-    return axios.post( `${ baseUrl }/${ endpoint }`, params, { headers: {'Authorization': token}} )
-    .then(response => {
-        console.log(response);
-    return response.data;
+    return axios.post(`${baseUrl}/${endpoint}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
 
-    })
+        })
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const updateProducts = ( endpoint, product ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
+export const updateProducts = (endpoint, product) => { // por el momento solo un parametro luego tendremos que mandar el token
+
     let json = JSON.stringify(product);
     let params = "json=" + json;
 
-    return axios.put( `${ baseUrl }/${ endpoint }`, params, { headers: {'Authorization': token}} )
-    .then(response => {
-        console.log(response);
-    return response.data;
+    return axios.put(`${baseUrl}/${endpoint}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
 
-    })
+        })
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const deleteProducts = ( endpoint ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
-    return axios.delete( `${ baseUrl }/${ endpoint }`, { headers: {'Authorization': token}} )
-    .then(response => {
-        console.log(response);
-    return response.data;
+export const deleteProducts = (endpoint) => { // por el momento solo un parametro luego tendremos que mandar el token
 
-    })
+    return axios.delete(`${baseUrl}/${endpoint}`, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const uploadProduct = ( endpoint, file0 ) => { // por el momento solo un parametro luego tendremos que mandar el token
-    
+export const uploadProduct = (endpoint, file0) => { // por el momento solo un parametro luego tendremos que mandar el token
+
     let formData = new FormData();
 
     formData.append("file0", file0);
 
-    return axios.post( `${ baseUrl }/${ endpoint }`, formData, { headers: {"Content-Type": "multipart/form-data",'Authorization': token}} )
-    .then(response => {
-        
-    return response.data;
+    return axios.post(`${baseUrl}/${endpoint}`, formData, { headers: { "Content-Type": "multipart/form-data", 'Authorization': token } })
+        .then(response => {
 
-    })
+            return response.data;
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const getProducts = ( endpoint ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
-    return axios.get( `${ baseUrl }/${ endpoint }`, { headers: {'Content-Type': 'application/json', 'Authorization': token}} )
-    .then(response => {
-        
-    return response.data;
+export const getProducts = (endpoint) => { // por el momento solo un parametro luego tendremos que mandar el token
 
-    })
+    return axios.get(`${baseUrl}/${endpoint}`, { headers: { 'Content-Type': 'application/json', 'Authorization': token } })
+        .then(response => {
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+            return response.data;
+
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const getMaterialByProduct = ( endpoint ) => { // por el momento solo un parametro luego tendremos que mandar el token
- 
-    return axios.get( `${ baseUrl }/${ endpoint }`, { headers: {'Content-Type': 'application/json', 'Authorization': token}} )
-    .then(response => {
-        
-    return response.data;
+export const getMaterialByProduct = (endpoint) => { // por el momento solo un parametro luego tendremos que mandar el token
 
-    })
+    return axios.get(`${baseUrl}/${endpoint}`, { headers: { 'Content-Type': 'application/json', 'Authorization': token } })
+        .then(response => {
 
-    .catch(error => {
-        console.log(error);
-        return error;
-    });            
+            return response.data;
+
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
--- src/services/shippingService.js
+++ src/services/shippingService.js
@@ -2,62 +2,62 @@ import axios from 'axios';
 
 const baseUrl = process.env.REACT_APP_API_URL;
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAxQ0VDMTkwLTBCMzgtMTFFQi1CRDg4LTIxQTJBOTE2RjJDNSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYwMjM2MjI1NCwiZXhwIjoxNjA0OTU0MjU0fQ.qe_inikq-F-2wNVbIAdmbTvNDZnhpA5c9RT31ALEBhU';
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjBCMzlBQ0MwLTIzRDYtMTFFQi05MzlELUUzNjc1MEQxRDlBMiIsInVzZXJuYW1lIjoiZHZwZXJnYSIsImVtYWlsIjoiZGllZ29AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjA1MDY4Nzk3LCJleHAiOjE2MDc2NjA3OTd9.ud-hY6kHE1O4a-dPyorw4PvHgzvF2B130y2xe1y6ozQ';
 
-export const storeShippings = ( endpoint, shipping ) => {
-
-    let json = JSON.stringify(shipping);
-    let params = "json=" + json;
-
-    return axios.post(`${ baseUrl }/${ endpoint }`, params, { headers: {'Authorization': token}} )
-    .then(response => {
-
-        return response.data;
-    })
-    .catch(error => {
-        console.log(error);
-        return error;
-    });
-}
-
-export const getShipping = ( endpoint ) => {
-    
-    return axios.get(`${ baseUrl }/${ endpoint }`, { headers: {'content-type': 'application/json', 'Authorization': token}} )
-    .then(response => {
-
-        return response.data;
-    })
-    .catch(error => {
-        console.log(error);
-        return error;
-    })
-}
-
-export const updateShipping = ( endpoint, shipping, id) => {
+export const storeShippings = (endpoint, shipping) => {
 
     let json = JSON.stringify(shipping);
     let params = "json=" + json;
 
-    return axios.put(`${ baseUrl }/${ endpoint }/${ id }`, params, { headers: {'Authorization': token}} )
-    .then(response => {
+    return axios.post(`${baseUrl}/${endpoint}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
 
-        return response.data;
-    })
-    .catch(error => {
-        console.log(error);
-        return error;
-    })
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
 
-export const deleteShipping = ( endpoint, id) => {
+export const getShipping = (endpoint) => {
 
-    return axios.delete(`${ baseUrl }/${ endpoint }/${ id }`, { headers: {'Authorization': token}} )
-    .then(response => {
+    return axios.get(`${baseUrl}/${endpoint}`, { headers: { 'content-type': 'application/json', 'Authorization': token } })
+        .then(response => {
 
-        return response.data;
-    })
-    .catch(error => {
-        console.log(error);
-        return error;
-    })
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        })
+}
+
+export const updateShipping = (endpoint, shipping, id) => {
+
+    let json = JSON.stringify(shipping);
+    let params = "json=" + json;
+
+    return axios.put(`${baseUrl}/${endpoint}/${id}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
+
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        })
+}
+
+export const deleteShipping = (endpoint, id) => {
+
+    return axios.delete(`${baseUrl}/${endpoint}/${id}`, { headers: { 'Authorization': token } })
+        .then(response => {
+
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        })
 }
