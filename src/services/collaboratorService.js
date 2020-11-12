0ae9189c3f449afb707860190abe5c96a35c3766
elvinlab <60045534+elvinlab@users.noreply.github.com>
2020-11-11 20:09:36 -0600

    CRUD collaborator listo

--- src/services/collaboratorService.js
+++ src/services/collaboratorService.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const baseUrl = process.env.REACT_APP_API_URL;
 
 //saliendo del apuro
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjAxQ0VDMTkwLTBCMzgtMTFFQi1CRDg4LTIxQTJBOTE2RjJDNSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYwMjM2MjI1NCwiZXhwIjoxNjA0OTU0MjU0fQ.qe_inikq-F-2wNVbIAdmbTvNDZnhpA5c9RT31ALEBhU';
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjBCMzlBQ0MwLTIzRDYtMTFFQi05MzlELUUzNjc1MEQxRDlBMiIsInVzZXJuYW1lIjoiZHZwZXJnYSIsImVtYWlsIjoiZGllZ29AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjA1MDY4Nzk3LCJleHAiOjE2MDc2NjA3OTd9.ud-hY6kHE1O4a-dPyorw4PvHgzvF2B130y2xe1y6ozQ';
 
 
 export const getCollaborators = ( endpoint ) => { // por el momento solo un parametro luego tendremos que mandar el token
  
     return axios.get( `${ baseUrl }/${ endpoint }`, { headers: {'Content-Type': 'application/json', 'Authorization': token}} )
     .then(response => {
-        
+        console.log(response);
     return response.data;
 
     })
@@ -18,5 +18,60 @@ export const getCollaborators = ( endpoint ) => { // por el momento solo un para
     .catch(error => {
         console.log(error);
         return error;
-    });            
+    });  
+}
+
+export const storeCollaborator = (endpoint, collaborator) => { // por el momento solo un parametro luego tendremos que mandar el token
+
+    let json = JSON.stringify(collaborator);
+    let params = "json=" + json;
+
+    console.log(`${baseUrl}/${endpoint}`);
+    console.log(params);
+    return axios.post(`${baseUrl}/${endpoint}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
+
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
+}
+
+export const updateCollaborator = (endpoint, collaborator) => { // por el momento solo un parametro luego tendremos que mandar el token
+
+    let json = JSON.stringify(collaborator);
+    let params = "json=" + json;
+
+    console.log(`${baseUrl}/${endpoint}`);
+    console.log(params);
+    return axios.put(`${baseUrl}/${endpoint}`, params, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
+
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
+}
+
+export const deleteCollaborator = (endpoint) => { // por el momento solo un parametro luego tendremos que mandar el token
+
+    return axios.delete(`${baseUrl}/${endpoint}`, { headers: { 'Authorization': token } })
+        .then(response => {
+            console.log(response);
+            return response.data;
+
+        })
+
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
